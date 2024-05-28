f666abf09487ddf65771272bfa9e3a3efb547899
Michael Binder <rednib.coding@gmail.com>
2024-05-28 22:05:23 +0200

    update example

--- example/main.c
+++ example/main.c
@@ -10,26 +10,16 @@ int main() {
 
     mgImage image = mgLoadImage("./assets/GLFW.png");
 
-    // Load the font
-    mgFont *font = mgLoadFont("./assets/Droid_Sans/DroidSans.ttf");
-
-    if (!font) {
-        printf("Failed to load font\n");
-        return -1;
-    }
 
     while (!mgWindowShouldClose()) {
         mgClear();
 
-        // mgDrawImage(image, (mgPointf){20.0f, 20.0f});
+        mgDrawImage(image, (mgPointf){20.0f, 20.0f});
 
-        // mgDrawImagePortion(image, (mgPointf){200.0f, 200.0f}, (mgRecf){0.0f, 0.0f, 50.0f, 50.0f});
-
-        mgDrawText(font, "Hello, World!", 100.0f, 100.0f, 60.0f);
+        mgDrawImagePortion(image, (mgPointf){200.0f, 200.0f}, (mgRecf){0.0f, 0.0f, 50.0f, 50.0f});
 
         mgSwap();
     }
 
-    // mgFreeFont(font);
     mgDestroyWindow();
 }
